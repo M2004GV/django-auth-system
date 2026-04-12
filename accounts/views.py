a067404eaaf64732f73a9535dcd993968155f46c
Lucas <garciamateus285@gmail.com>
2026-04-12 19:37:09 -0300

    feat: implementar ativação de conta por email com token

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.sites.shortcuts import get_current_site
+from django.shortcuts import redirect
+from django.template.loader import render_to_string
+from django.urls import reverse_lazy
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.views.generic import CreateView, TemplateView, View
+from django.core.mail import EmailMessage
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from .forms import CustomUserCreationForm
+
+User = get_user_model()
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = "registration/signup.html"
+    success_url = reverse_lazy("login")
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+
+        current_site = get_current_site(self.request)
+        subject = "Confirme sua conta"
+        message = render_to_string("registration/account_activation_email.html", {
+            "user": user,
+            "domain": current_site.domain,
+            "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+            "token": default_token_generator.make_token(user),
+            "protocol": "https" if self.request.is_secure() else "http",
+        })
+
+        email = EmailMessage(subject, message, to=[user.email])
+        email.send()
+
+        messages.success(
+            self.request,
+            "Conta criada com sucesso. Verifique seu e-mail para ativar a conta."
+        )
+        return redirect("login")
+
+
+class ActivateAccountView(View):
+    def get(self, request, uidb64, token):
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        if user is not None and default_token_generator.check_token(user, token):
+            user.is_active = True
+            user.save()
+            messages.success(request, "Conta ativada com sucesso. Agora faça login.")
+        else:
+            messages.error(request, "Link de ativação inválido ou expirado.")
+
+        return redirect("login")
+
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = "dashboard.html"
--- core/urls.py
+++ core/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
     path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path("reset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 
+    path("activate/<uidb64>/<token>/", ActivateAccountView.as_view(), name="activate"),
     path("dashboard/", DashboardView.as_view(), name="dashboard"),
 ]
